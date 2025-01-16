67dec6054e70c499cf6f8817ca9fde07ea7d500d
Zayed <s443010753@uqu.edu.sa>
2025-01-17 00:14:04 +0300

    add the 4 pages and create the app bar and add to to all pages

---
+++ lib/zayed/Screens/zayed_leaderboard_page.dart
@@ -0,0 +1,56 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Leaderboard_page());
+}
+
+class Leaderboard_page extends StatelessWidget {
+  const Leaderboard_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFe4e4e4),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFf1f1f1),
+          centerTitle: true,
+           toolbarHeight: 80.0, // Adjust AppBar height
+           leading: Padding(
+              padding: const EdgeInsets.only(bottom: 1.0), // Adjust this value
+              child: IconButton(
+                icon: const Icon(
+                  Icons.menu, // List/Menu icon
+                  size: 47.0,
+                  color: Color.fromARGB(197, 0, 129, 189),
+                ),
+                onPressed: () {}, // Add your logic here
+              ),
+           ),
+          title: Text("Gamify",
+            style: TextStyle(
+            color: Color.fromARGB(255, 6, 147, 198),
+            fontSize: 33.0,
+            ),
+          ),        
+          actions: <Widget>[
+           Padding(
+              padding: const EdgeInsets.only(top: 6.0), // Adjust this value
+              child: IconButton(
+                icon: const Icon(
+                  Icons.notifications_none,
+                  size: 34.0,
+                  color: Color.fromARGB(197, 0, 129, 189),
+                ),
+                onPressed: () {},
+              ),
+          )
+        ],
+        ),
+      ),
+    );
+  }
+}
