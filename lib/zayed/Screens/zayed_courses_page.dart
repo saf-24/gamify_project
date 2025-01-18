68aed3c6880d320f0a7144e8b63df2916ced3b4e
Zayed <s443010753@uqu.edu.sa>
2025-01-18 16:52:25 +0300

    shof finish courses page khalas @_< without any problem

--- lib/zayed/Screens/zayed_courses_page.dart
+++ lib/zayed/Screens/zayed_courses_page.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:gamify_project/Safwan/Screens/dummy_data.dart';
+import 'package:gamify_project/zayed/Screens/zayed_courses_cards.dart';
 
 void main() {
   runApp(const Zayed_standard_navigations());
@@ -17,7 +19,7 @@ class Zayed_standard_navigations extends StatelessWidget {
         backgroundColor: const Color(0xFFe4e4e4),
         // appbar ://------------------------------------------//
         appBar: AppBar(
-          backgroundColor: const Color(0xFFf1f1f1),
+          backgroundColor: const Color.fromARGB(255, 244, 243, 243),
           centerTitle: true,
           toolbarHeight: 80.0, // Adjust AppBar height
           elevation: 4.0,
@@ -66,7 +68,38 @@ class Zayed_standard_navigations extends StatelessWidget {
 
         // Main content of the page better be here : ////////////-----------//////////------
 
-        
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 7, 0, 0),
+                child: const Text("Courses",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 3,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.71,
+              child: ListView.builder(
+                itemCount: test2.length,
+                itemBuilder: (context, index) {
+                  return Courses_cards(
+                    title: test2[index]['title']!,
+                    disc: test2[index]['progres']!,
+                    date: test2[index]['lessonName']!,
+                    percent1: test3[index]['percent']!,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
 
         // Custom Bottom Taskbar///////////---------------//////////-------
 
